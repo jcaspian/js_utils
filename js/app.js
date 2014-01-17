51af8d9a05daa4e69ddf488b4851196df4bdccc1
jcaspian <jeff_caspian@hotmail.com>
2014-01-17 15:20:52 +0800

    qweqweqe

--- js/app.js
+++ js/app.js
@@ -64,7 +64,7 @@ var app = {
         success: function (res, status, xhr) {
           var content = app.element.content.detach().empty();
           app.element.container.append(content.append(res));
-          //app.fn.bindEvent();
+          app.fn.bindEvent();
         }
       });
     },
@@ -73,7 +73,6 @@ var app = {
       
       app.fn.ajaxSetup();
       app.fn.loadPage();
-      //app.fn.bindEvent();
     }
   }
 };
