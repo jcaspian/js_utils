4e754b430c767e2a1d2c7e7ede75d35f550ff839
jcaspian <jeff_caspian@hotmail.com>
2014-01-17 09:00:56 +0800

    .;/l;

---
+++ js/jumper.js
@@ -0,0 +1,1 @@
+window.jsBootstrap || (location.href = [location.origin,location.pathname.toString().substring(location.pathname.toString().indexOf('/'),location.pathname.toString().lastIndexOf('/')+1),'?page=',location.pathname.toString().substring(location.pathname.toString().lastIndexOf('/')+1,location.pathname.toString().lastIndexOf('.')),'&',location.search.substring(1)].join(''));
